4de161e310059f4119f828aaa4ae13f9e200d422
Robert Bartlensky <bartlensky.robert@gmail.com>
2022-08-21 02:52:32 +0100

    Ensure safe is locked even if dmenu fails.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use anyhow::{anyhow, bail};
+use anyhow::{anyhow, bail, Context};
 use copypasta::{ClipboardContext, ClipboardProvider};
 use nitrokey::{Device, GetPasswordSafe, PasswordSafe};
 use pinentry::PassphraseInput;
@@ -27,15 +27,15 @@ fn dmenu(choices: &[String]) -> anyhow::Result<String> {
         .write_all(choices.join("\n").as_bytes())?;
     let output = dmenu.wait_with_output()?;
     if !output.status.success() {
-        bail!("failed to run 'dmenu'");
+        bail!("'dmenu' exited with {}", output.status);
     }
     let choice = String::from_utf8(output.stdout)?;
     Ok(choice.trim_end_matches('\n').into())
 }
 
 fn main() -> anyhow::Result<()> {
-    let mut manager = nitrokey::take()?;
-    let mut device = manager.connect()?;
+    let mut manager = nitrokey::take().context("failed to get device")?;
+    let mut device = manager.connect().context("failed to connect to device")?;
     let data = PassphraseInput::with_default_binary()
         .ok_or_else(|| anyhow!("no 'pinentry' command found"))?
         .with_prompt("Nitrokey auth")
@@ -45,6 +45,12 @@ fn main() -> anyhow::Result<()> {
         ))
         .interact()
         .map_err(|e| anyhow!("no password provided: {}", e))?;
+    let r = show_dmenu(&mut device, &data);
+    device.lock().context("failed to lock device")?;
+    r
+}
+
+fn show_dmenu(device: &mut nitrokey::DeviceWrapper, data: &secrecy::SecretString) -> anyhow::Result<()> {
     let safe = device.get_password_safe(data.expose_secret())?;
     let r = slots(&safe);
     if let Ok(slots) = &r {
@@ -56,7 +62,5 @@ fn main() -> anyhow::Result<()> {
         ctx.set_contents(pw).map_err(|e| anyhow!("failed to copy password to clipboard: {}", e))?;
         sleep(Duration::from_secs(5));
     }
-    drop(safe);
-    device.lock()?;
     r.map(|_| ())
 }
